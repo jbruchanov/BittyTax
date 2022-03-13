c7bca26e4ff1ee42e75b9fa69b4338df2572ca5a
Jiri Bruchanov <jbruchanov@gmail.com>
2022-03-13 16:10:08 +0000

    Improve trezorsuite to support tokens on eth

--- bittytax/conv/parsers/trezorsuite.py
+++ bittytax/conv/parsers/trezorsuite.py
@@ -3,11 +3,14 @@
 
 import re
 
+from decimal import Decimal
+
 from ..out_record import TransactionOutRecord
 from ..dataparser import DataParser
-from ..exceptions import UnknownCryptoassetError, UnexpectedTypeError
+from ..exceptions import UnknownCryptoassetError, UnexpectedTypeError, DataParserError
 
 WALLET = "Trezor"
+
 
 def parse_trezor_suite(data_row, parser, **kwargs):
     row_dict = data_row.row_dict
@@ -26,11 +29,25 @@ def parse_trezor_suite(data_row, parser, **kwargs):
 
     if row_dict['Type'] == "RECV":
         # Workaround: we have to ignore the fee as fee is for the sender
-        data_row.t_record = TransactionOutRecord(TransactionOutRecord.TYPE_DEPOSIT,
-                                                 data_row.timestamp,
-                                                 buy_quantity=row_dict['Total'],
-                                                 buy_asset=symbol,
-                                                 wallet=WALLET)
+        if Decimal(row_dict['Total']) == 0.0:
+            matches = re.findall(r"\((.*) (.*)\)", row_dict['Addresses'])[0]
+            if matches is None:
+                raise DataParserError(kwargs['filename'], kwargs.get('worksheet'))
+            quantity = Decimal(matches[0])
+            symbol_in = matches[1]
+            if symbol_in != symbol:
+                raise DataParserError(kwargs['filename'], kwargs.get('worksheet'))
+            data_row.t_record = TransactionOutRecord(TransactionOutRecord.TYPE_DEPOSIT,
+                                                     data_row.timestamp,
+                                                     buy_quantity=quantity,
+                                                     buy_asset=symbol,
+                                                     wallet=WALLET)
+        else:
+            data_row.t_record = TransactionOutRecord(TransactionOutRecord.TYPE_DEPOSIT,
+                                                     data_row.timestamp,
+                                                     buy_quantity=row_dict['Total'],
+                                                     buy_asset=symbol,
+                                                     wallet=WALLET)
     elif row_dict['Type'] == "SENT":
         data_row.t_record = TransactionOutRecord(TransactionOutRecord.TYPE_WITHDRAWAL,
                                                  data_row.timestamp,
@@ -58,6 +75,7 @@ def parse_trezor_suite(data_row, parser, **kwargs):
                                                  note="Failure")
     else:
         raise UnexpectedTypeError(parser.in_header.index('Type'), 'Type', row_dict['Type'])
+
 
 DataParser(DataParser.TYPE_WALLET,
            "Trezor Suite",
